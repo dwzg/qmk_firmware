ca57dc129468905c2eb9f134b57419dc76f68027
Dennis Witzig <dennis@wtzg.de>
2020-06-08 21:05:12 +0200

    Update config and clean up keymap

--- keyboards/dwzg72/config.h
+++ keyboards/dwzg72/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID 0xFEED
-#define PRODUCT_ID 0x0000
+#define PRODUCT_ID 0xd72a
 #define DEVICE_VER 0x0001
 #define MANUFACTURER Dennis Witzig
 #define PRODUCT dwzg72
@@ -51,7 +51,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /*
  * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
  */
-#define SOFT_SERIAL_PIN E6  // or D1, D2, D3, E6
+// #define SOFT_SERIAL_PIN E6  // or D1, D2, D3, E6
 
 // #define BACKLIGHT_PIN B7
 // #define BACKLIGHT_BREATHING
